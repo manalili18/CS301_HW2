c36ad91033468e7abaf260132bbcc37346fcfb86
manalili18 <manalili18@up.edu>
2018-02-12 15:44:57 -0800

    listeners kind of work, focus on the listener class next time you update

--- app/src/main/java/com/example/manalili18/cs301_hw2/MainActivity.java
+++ app/src/main/java/com/example/manalili18/cs301_hw2/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,21 +30,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         face = (Face) findViewById(R.id.surfaceView);
         listener = new MyListener();
-
+/*
         randomBut = (Button) findViewById(R.id.button);
         randomBut.setOnClickListener(listener);
+*/
 
+
+        listener.addTV((TextView)findViewById(R.id.redVal));
+        listener.addTV((TextView)findViewById(R.id.greenVal));
+        listener.addTV((TextView)findViewById(R.id.blueVal));
+
+
+/*
         hairSpin = (Spinner) findViewById(R.id.spinner);
         hairSpin.setOnItemClickListener(listener);
-
+*/
         redSb = (SeekBar) findViewById(R.id.red);
+        redSb.setOnSeekBarChangeListener(listener);
         greenSb = (SeekBar) findViewById(R.id.green);
+        greenSb.setOnSeekBarChangeListener(listener);
         blueSb = (SeekBar) findViewById(R.id.blue);
+        blueSb.setOnSeekBarChangeListener(listener);
 
+        /*
         hair = (RadioButton) findViewById(R.id.hair);
+        hair.setOnClickListener(listener);
         eyes = (RadioButton) findViewById(R.id.eyes);
+        eyes.setOnClickListener(listener);
         skin = (RadioButton) findViewById(R.id.skin);
+        skin.setOnClickListener(listener);
+        */
+
     }
 }
