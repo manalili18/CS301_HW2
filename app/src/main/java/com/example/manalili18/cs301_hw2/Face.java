549cdc638c831597297214439b4adf5f1a061d37
manalili18 <manalili18@up.edu>
2018-02-12 16:00:56 -0800

    TODO: Nest Listener in Main Activity

--- app/src/main/java/com/example/manalili18/cs301_hw2/Face.java
+++ app/src/main/java/com/example/manalili18/cs301_hw2/Face.java
@@ -28,6 +28,7 @@ public class Face extends SurfaceView {
     }
 
     private int skinColor, eyeColor, hairColor, hairStyle;
+    private Paint skinPaint, eyePaint, hairPaint;
     public static Random random;
 
     public Face(Context context) {
@@ -53,22 +54,36 @@ public class Face extends SurfaceView {
         // https://stackoverflow.com/questions/6202818/initializing-multiple-variables-to-the-same-value-in-java
         skinColor = eyeColor = hairColor = hairStyle = 0; // stack overflow
 
+        skinPaint = new Paint();
+        eyePaint = new Paint();
+        hairPaint = new Paint();
+
         random = new Random();
 
         randomize();
     }
 
     public void randomize() {
-        skinColor = OPAQUE + random.nextInt(MAX_COLOR+1); // TODO: consider putting bounds on this
-        eyeColor = OPAQUE + random.nextInt(MAX_COLOR+1);
-        hairColor = OPAQUE + random.nextInt(MAX_COLOR+1);
+        skinColor = OPAQUE + random.nextInt(MAX_COLOR + 1);
+        eyeColor = OPAQUE + random.nextInt(MAX_COLOR + 1);
+        hairColor = OPAQUE + random.nextInt(MAX_COLOR + 1);
         hairStyle = random.nextInt(MAX_HAIR_STYLE);
+
+        skinPaint.setColor(skinColor);
+        eyePaint.setColor(eyeColor);
+        hairPaint.setColor(hairColor);
+
     }
 
     public void onDraw(Canvas c) {
         //draw face
         Paint temp = new Paint();
         temp.setColor(Color.BLUE);
-        c.drawCircle(this.getWidth()/2,this.getHeight()/2,20,temp);
+        c.drawCircle(this.getWidth() / 2,
+                this.getHeight() * 2 / 3,
+                (this.getWidth() > this.getHeight()) ? this.getHeight() * 2 / 3 : this.getWidth() * 2 / 3,
+                skinPaint);
     }
+
 }
+
